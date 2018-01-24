02cda6485e0654cb422abd764c531b439a983c2a
rocketman1974 <34247541+rocketman1974@users.noreply.github.com>
2018-01-23 21:50:26 -0500

    Add files via upload
    Sets altitude and VNAV via preset altitudes, set as INTs. Also testing major getstate methods.

---
+++ AltitudePrint.cs
@@ -0,0 +1,24 @@
+﻿using Fds.IFAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class AltitudePrint
+{
+	public printToConsole() {
+        /// This tests the major getstate methods required for the FMC
+        Console.Write("The current aircraft status is: " + airplane.getstate);
+        Console.Write("The AutoPilot (AP) is currently: " + autopilot.getstate);
+	}
+    public changeAltitude()
+    {
+        /// This code is to set a specific altitude on the autopilot and enable the VNAV
+        int testaltitude1 = 29000;
+        int testaltitude2 = 33000;
+        int testaltitude3 = 1000;
+        int testaltitude4 = 50000;
+        commands.autopilot.setaltitude(testaltitude1);
+        commands.autopilot.setaltitudestate(on);
+    }
+}
